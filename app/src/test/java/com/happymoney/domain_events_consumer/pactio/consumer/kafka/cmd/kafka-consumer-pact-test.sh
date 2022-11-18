5ed50d2aae991102db8c7fed1c80b71335c593b5
Jackhman Thi Ysa <jysa@happymoney.com>
2022-11-18 10:58:15 -0800

    Update script

--- app/src/test/java/com/happymoney/domain_events_consumer/pactio/consumer/kafka/cmd/kafka-consumer-pact-test.sh
+++ app/src/test/java/com/happymoney/domain_events_consumer/pactio/consumer/kafka/cmd/kafka-consumer-pact-test.sh
@@ -1,5 +1,4 @@
-#./gradlew test --tests KafkaConsumerPactTest \
-#pactPublish \
-#canideploy -Ppacticipant='ApiVerifyProvider' -PpacticipantVersion=0.0.1-SNAPSHOT \
-#-Ppacticipant='DomainEventsConsumer' -PpacticipantVersion=0.0.1-SNAPSHOT
-./gradlew test --tests KafkaConsumerPactTest
+./gradlew test --tests KafkaConsumerPactTest \
+pactPublish \
+canideploy -Ppacticipant='ApiVerifyProvider' -PpacticipantVersion=0.0.1-SNAPSHOT \
+-Ppacticipant='DomainEventsConsumer' -PpacticipantVersion=0.0.1-SNAPSHOT
